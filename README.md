774425b89b2accc62a1d181bafff5eedfebd9503
Elena Christodoulou <elena.c@live.co.uk>
2018-09-29 11:52:21 +0100

    version 2

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# home
+# Version2
--- js/script.js
+++ js/script.js
@@ -1,21 +1,55 @@
-let mainNav=document.getElementById('main-nav');
-let navbarToggle=document.getElementById('navbar-toggle');
+//adds active nav when scrolling down
 
-navbarToggle.addEventListener('click',function(){
+var sections = $('section')
+  , nav = $('nav')
+  , nav_height = nav.outerHeight();
 
-    if(this.classList.contains('active')){
-        mainNav.style.display="none";
-        this.classList.remove('active');
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+  
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height,
+        bottom = top + $(this).outerHeight();
+    
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+      
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
     }
-    else{
-        mainNav.style.display="flex";
-        this.classList.add('active');
+  });
+});
 
-    }
+nav.find('a').on('click', function () {
+  var $el = $(this)
+    , id = $el.attr('href');
+  
+  $('html, body').animate({
+    scrollTop: $(id).offset().top - nav_height
+  }, 500);
+  
+  return false;
 });
 
 
 
+
+
+
+
+//nav change colour when scroll down
+
+$(function () {
+    $(document).scroll(function () {
+      var $nav = $(".navbar");
+      var $logo = $("#logo");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+      $logo.toggleClass('logoscroll', $(this).scrollTop() > $logo.height());
+    });
+  })
+
+//slide show 
 
 var slideIndex = 1;
 showSlides(slideIndex);
@@ -45,6 +79,3 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active-dot";
 }
-
-
-
